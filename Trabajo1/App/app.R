f950569fae2bd8dac16df43581386bcbabd34ce2
saarboledaz <sergioarboledazuluaga@gmail.com>
2020-11-15 22:37:51 -0500

    Datos históricos y predicción lista

--- Trabajo1/App/app.R
+++ Trabajo1/App/app.R
@@ -1,9 +1,12 @@
 library(shiny)
 library(ggplot2)
 
-load("./model.RData")
-load("./days_df.RData")
-load("./clases.RData")
+Sys.setlocale(locale="spanish") # Idioma en Español para windows
+Sys.setlocale(locale="es") # Idioma en Español para linux
+load("./model.RData") # Modelo para predecir
+load("./days_df.RData") # Dataframe de días pre-calculados para el modelo 
+load("./clases.RData") # Clases precargadas, usadas en el modelo
+load("./historicos_df.RData") # Datos históricos
 
 # Función para obtener la fecha final
 resolveEnd <- function(dateStart, timeUnit, timeWindow){
@@ -13,7 +16,7 @@ resolveEnd <- function(dateStart, timeUnit, timeWindow){
 
 # Procesa los resultados y los entrega para su visualización, dependiendo de la
 # ventana de tiempo escogida
-processResults <- function(timeUnit, timeWindow, dateStart, dateEnd, results){
+processResults <- function(timeUnit, dateStart, dateEnd, results){
   # Días
   if(timeUnit == 1){
     # Si son días, no se tienen que procesar los datos
@@ -23,12 +26,13 @@ processResults <- function(timeUnit, timeWindow, dateStart, dateEnd, results){
     
     return(processedResults)
   }else{
+    # Si no son días, se tienen que sumarizar los datos por la unidad de tiempo escogida
     numPoints <- ceiling(length(results)/timeUnit) # Número de puntos
     newResults <- integer(numPoints) # Arreglo de enteros
     count <- 1
     for(i in seq(from = 1, to = length(results), by = timeUnit)){
       limit <-i+timeUnit
-      if ((limit) > length(results)+1){
+      if ((limit) > length(results)+1){ # Esto para evitar indices fuera de borde
         limit <- length(results)
       }
       limit <- limit -1
@@ -37,6 +41,8 @@ processResults <- function(timeUnit, timeWindow, dateStart, dateEnd, results){
     }
     dates <- seq.Date(from = dateStart,to = dateEnd, by= timeUnit)
     results_df <- data.frame(value = newResults, fecha = dates)
+    
+    # Resolver cadenas de unidad de tiempo
     timeString <- ""
     timeStringPlural <- ""
     if (timeUnit == 7){
@@ -62,23 +68,34 @@ ui <- fluidPage(
     # Datos Históricos
     tabPanel(
       title = "Datos históricos",
+      hr(),
+      helpText("Es importante notar que para la unidad de tiempo se asume, que las semanas son igual a 7 días y los meses a 30, por
+      esto es posible obtener puntos que son más bajos ya que como tal no encierran meses exactos."),
       sidebarPanel(
         # Rango de Fechas
         dateRangeInput('dateRange',
                        label="Rango de Fechas", 
                        start = "2014-01-01", 
-                       end ="2018-12-31", 
+                       end ="2014-12-31", 
                        language = "es", 
-                       separator = "hasta", 
+                       separator = "a", 
                        min = "2014-01-01",
                        max = "2018-12-31"),
+        # Unidad de tiempo (día, meses, años)
+        selectInput('hist_timeUnit', 
+                    label="Unidad de tiempo",
+                    choices=list("Días" =1, "Semanas" = 7, "Meses" = 30), 
+                    selected = 30),
         # Tipo de accidente
-        selectInput('hist_accType', 
+        selectInput('hist_accClass', 
                     label="Tipo de Accidente",
-                    choices=list("Tipo 1" =1, "Tipo 2" = 2), 
-                    selected = 1)
+                    choices=clases, 
+                    selected = "Atropello")
       ),
-      mainPanel()
+      mainPanel(
+        plotOutput('accHistPlot'),
+        textOutput('accHistCount')
+      )
     ),
     
     # Predicción
@@ -88,7 +105,9 @@ ui <- fluidPage(
       helpText("La predicción solo es posible realizarla desde el ",
       strong("1 de Enero de 2019"),
       "hasta el ",
-      strong("31 de Diciembre de 2020")),
+      strong("31 de Diciembre de 2020."),
+      "Es importante notar que para la unidad de tiempo se asume, que las semanas son igual a 7 días y los meses a 30, por
+      esto es posible obtener puntos que son más bajos ya que como tal no encierran meses exactos."),
       sidebarPanel(
         # Fecha de inicio de la predicción
         dateInput('dateStart', 
@@ -116,9 +135,7 @@ ui <- fluidPage(
                     selected = "Atropello")
         
       ),
-      # TODO: Gráfica y resultados de la predicción
       mainPanel(
-        
         plotOutput('accPredictPlot'),
         textOutput('accPredictCount')
       )
@@ -142,15 +159,15 @@ ui <- fluidPage(
 )
 
 # Define la lógica del servidor
-server <- function(input, output) {
-  # TODO: Definir las salidas 
+server <- function(input, output, session) {
   
+  # Gráfica de predicción
   output$accPredictPlot <- renderPlot({
     timeUnit <- as.integer(input$timeUnit)
     timeWindow <- as.integer(input$timeWindow)
     dateStart <- as.Date(input$dateStart)
     dateEnd <- resolveEnd(dateStart,timeUnit,timeWindow)
-    dateStart <- dateStart + 1
+    dateStart <- dateStart + 1 # No incluir la fecha inicial
     # Validar que 'dateEnd' sea menor al 2020-12-31
     if (dateEnd > "2020-12-31"){
       dateEnd = as.Date("2020-12-31")
@@ -165,7 +182,8 @@ server <- function(input, output) {
     
     results <- round(predict(model, newdata= predictionSubset, type="response"))
     
-    pResults <- processResults(timeUnit, timeWindow, dateStart, dateEnd, results)
+    # Procesar los datos según la unidad de tiempo
+    pResults <- processResults(timeUnit, dateStart, dateEnd, results)
     
     values <- pResults$results_df
     timeString <- pResults$timeString
@@ -181,10 +199,37 @@ server <- function(input, output) {
     predictionPlot + ggtitle("Predicción de Accidentalidad en Medellín (2019-2020)", subtitle=sprintf("Con %i Registros, %i %s",length(results), length(values$value), timeStringP))+ ylab("No. de Accidentes") + xlab("Fecha")
   })
   
-
-  
-
-  
+  # Gráfica de datos históricos
+  output$accHistPlot <- renderPlot({
+    dateRange <- as.Date(input$dateRange)
+    dateStart <- dateRange[1]
+    dateEnd <- dateRange[2]
+    timeUnit <- as.integer(input$hist_timeUnit)
+    accClass <- input$hist_accClass
+    
+    # Validar las fechas
+    
+    if (dateStart >= dateEnd){
+      showNotification("La fecha final no puede estar antes de la fecha incial, se extenderá la fecha inicial 30 días.", duration =  10, type = "warning")
+      
+      dateEnd <- dateStart + 30
+      
+      updateDateRangeInput(session, 'dateRange', end= dateEnd)
+    }
+    
+    values <- subset(historicos_df,(CLASE == accClass & FECHA >= dateStart & FECHA <= dateEnd))
+    pResults <- processResults(timeUnit, dateStart, dateEnd, values$NRO_ACC)
+    newValues <- pResults$results_df
+    timeString <- pResults$timeString
+    timeStringP <- pResults$timeStringPlural
+    
+    output$accHistCount <- renderText({
+      x <- sprintf("Número de Accidentes: %i, Promedio de Accidentes por %s: %g", sum(newValues$value),timeString,mean(newValues$value))
+      
+    })
+    histPlot <- ggplot() + geom_point(mapping = aes(newValues$fecha, newValues$value)) + geom_line(mapping = aes(newValues$fecha, newValues$value))
+    histPlot + ggtitle(sprintf("Número de accidentes de tipo %s por %s", accClass, timeString)) + xlab("Fecha") + ylab("Número de Accidentes")
+  })
 }
 
 # Crear la app de Shiny
